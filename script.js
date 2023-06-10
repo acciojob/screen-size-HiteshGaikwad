13b38c38faa7061d1831929369dd5dffd8bf5cd8
HiteshGaikwad <116432379+HiteshGaikwad@users.noreply.github.com>
2023-06-10 12:29:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let div= document.getElementById("main");
 
 function fun() {
 	let h= document.createElement("h1");
-	h.innerHtml=`
+	h.innerHTML=`
  <h1>Width: ${window.width} and Height: ${window.height}</h1>
  `
 	div.append(h);
